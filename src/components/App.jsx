486fe161924c6cff87d059d994ec1fa56feaaddf
VictoriaSzn <victoriia.szn@gmail.com>
2023-03-17 00:02:41 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,19 +6,19 @@ import { nanoid } from 'nanoid';
 
 export function App() {
   const [filter, setFilter] = useState('');
-     const [contacts, setContacts] = useState([
-       {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-       {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-       {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-       {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}
-     ]) 
+    //  const [contacts, setContacts] = useState([
+    //    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    //    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    //    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    //    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}
+    //  ]) 
   
-    // const [contacts, setContacts] = useState(() => {
-    //   const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
-    //   if (parsedContacts) {
-    //     return parsedContacts ;
-    //   }
-    // });
+     const [contacts, setContacts] = useState(() => {
+       const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+       if (parsedContacts) {
+         return parsedContacts ;
+       }
+     });
 
   // const [contacts, setContacts] = useState(() => {
   //   return JSON.parse(window.localStorage.getItem('contacts')) ?? '';
